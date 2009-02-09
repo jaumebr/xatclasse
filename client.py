f93689b68cf5ff720c35909cbdc5222cd8ebc2f0
jaumebr <jaume@dellport.(none)>
2009-02-09 10:08:35 +0100

    Client - Servidor --> Punt de partida

--- client.py
+++ client.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
  
+import socket
+dgramSock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+dgramSock.sendto( "Hello World\n", ('', 8080) )
+print dgramSock.recv( 100 )
+dgramSock.close()
+
--- servidor.py
+++ servidor.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+import socket
+dgramSock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+dgramSock.bind( ('', 8080) )
+while 1:
+  msg, (addr, port) = dgramSock.recvfrom( 100 )
+  dgramSock.sendto( msg, (addr, port) )
+  print msg
