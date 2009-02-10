d6b15b25c1d7e6c2201cdaa0de9784a1d1a5c388
jaumebr <jaume@dellport.(none)>
2009-02-10 18:28:02 +0100

    Client - Servidor --> missatges indefinits fins a quit

--- client.py
+++ client.py
@@ -2,8 +2,16 @@
 # -*- coding: utf-8 -*-
  
 import socket
-dgramSock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
-dgramSock.sendto( "Hello World\n", ('', 8080) )
-print dgramSock.recv( 100 )
-dgramSock.close()
 
+def enviarMissatge(linia):
+  dgramSock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+  dgramSock.connect(("127.0.0.1",8080))
+  dgramSock.sendto( linia + "\n", ('', 8080) )
+  dgramSock.close()
+
+lin = ""
+while (lin <> "quit!"):
+  lin = raw_input("Enviar missatge: ")
+  enviarMissatge(lin)
+
+#enviarMissatge("quit!")
--- servidor.py
+++ servidor.py
@@ -2,9 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import socket
-dgramSock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+dgramSock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 dgramSock.bind( ('', 8080) )
-while 1:
-  msg, (addr, port) = dgramSock.recvfrom( 100 )
-  dgramSock.sendto( msg, (addr, port) )
+dgramSock.listen(10)
+msg2 = ""
+while (msg2 <> "quit!"):
+  (addr, port) = dgramSock.accept()
+#dgramSock.recvfrom( 100 )
+  #dgramSock.sendto( msg, (addr, port) )
+  #msg = addr.recv(100)
+  msg = addr.recv(300)
   print msg
+  addr.close()
+  msg2 = msg[0:5]
+  #print msg2
+dgramSock.close()
